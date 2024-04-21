8a8926f65decae2b45ce2a089c988922874d6ab5
Ata Mahmoudi <33548341+atamg@users.noreply.github.com>
2024-04-21 14:38:19 +0000

    Red button implemented, changes applied to deployments to meet limitation

--- watcher.sh
+++ watcher.sh
@@ -1,1 +1,26 @@
-#TODO
+#!/bin/bash
+RED='\033[0;31m'
+NOCOLOR='\033[0m'
+
+NS="sre"
+
+D_E="swype-app"
+
+MAX_RESTART=3
+
+while true; 
+do
+RESTARTS=$(kubectl get pods -n ${NS} -l app=${D_E} -o jsonpath="{.items[0].status.containerStatuses[0].restartCount}")
+echo "Number of restarts: ${RESTARTS}"
+if (( RESTARTS > MAX_RESTART )); 
+then
+echo "Maximum number of restarts exceeded. RED button executed..."
+kubectl scale --replicas=0 deployment/${D_E} -n ${NS}
+break
+fi
+
+sleep 60
+
+done
+
+
